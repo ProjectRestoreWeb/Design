8b53adda3d30d79864cc18e2960b40d268c3f2bd
NithinD1 <43255004+NithinD1@users.noreply.github.com>
2019-05-18 10:12:43 -0700

    init index.css, init index.html, init index.js

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
-//Sign Out of Account
-firebase.auth().signOut().then(function()
-{
-console.log("signed out");
-}).catch(function(error) {
-// An error happened.
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    console.log("Logged in");
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
 });
 
-function createUser()
-{
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("pass").value;
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorCode + " " + errorMessage);
-    });
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+}
+function createUserWithEmailAndPassword(){
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error : " + errorMessage);
+    // ...
+  });
+}
+
+
+function logout(){
+  firebase.auth().signOut();
+  console.log("Signed out");
 }
